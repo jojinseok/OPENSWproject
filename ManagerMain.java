6a7530040d73f8aca4c29fdc23756082734420d2
jojinseok <82083624+jojinseok@users.noreply.github.com>
2021-05-19 12:02:14 +0900

    Update ManagerMain.java

--- ManagerMain.java
+++ ManagerMain.java
@@ -23,34 +23,55 @@ ManagerMain(){
 			}
 		});
 	 JButton jb1=new JButton("회원 삭제");
-		jb1=new JButton("회원 정보 수정");
 		jb1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
 	 JButton jb2=new JButton("회원 수정");
-		jb2=new JButton("회원 정보 수정");
 		jb2.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
 	 JButton jb3=new JButton("좌석 관리");
-		jb3=new JButton("회원 정보 수정");
 		jb3.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				
 			}
 		});
-
+		JButton abcd= new JButton("로그아웃");
+		 abcd.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+					new Manager();	
+			}
+		 });
+		 JButton abcd11= new JButton("사용자 화면으로");
+		 abcd11.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+					new Login();	
+			}
+		 });
+		 JButton abcd1= new JButton("종료");
+		 abcd1.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e) {
+					setVisible(false);
+					System.exit(0);
+			}
+		 });
 		 c.setLayout(new BorderLayout());
 	 q1.add(jb);
 	 q1.add(jb1);
 	 q1.add(jb2);
 	 q1.add(jb3);
+	 q1.add(abcd);
+	 q1.add(abcd1);
+	 q1.add(abcd11);
 	 c.add(q1);
 	 setSize(400,500);
 	 setVisible(true);
 }
 }
+
